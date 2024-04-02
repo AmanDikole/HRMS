ef9bc970e51c40c2d513b9388b2a5770727171df
Ce22cs <shindesara444@gmail.com>
2024-04-02 19:43:18 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './component/Login'
+import Admin_main_box from './component/Admin_main_box'
 import LeftMenu from './component/leftMenu'
 
 function App() {
@@ -11,7 +9,12 @@ function App() {
   return (
     <>
     {/* <Login/> */}
+    <div className='dashboard_admin'>
+
     <LeftMenu/>
+    <Admin_main_box/>
+    <signUp/>    
+    </div>
     </>
   )
 }
---
+++ src/component/signUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function signUp() {
+return (
+    <><div>
+        aman</div></>
+    )
+}
+
+export default signUp

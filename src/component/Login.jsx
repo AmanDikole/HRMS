eddd0481054998046c464c7ebd4b7f4e38f70c56
shubham kasture <shubhamkasture289@gmial.com>
2024-04-02 14:19:07 +0530

    login page

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -10,19 +10,26 @@ const Login = () => {
                     <div className="login_form_title">Log in Your Account </div>
                     <form className="login_form">
                         <div className="login_form_input">
-                            <input type="text" className="login_form_input_text" placeholder="Email" />
+                            <input type="text" className="login_form_input_text" placeholder="Username" />
                         </div>
                         <div className="login_form_input">
                             <input type="password" className="login_form_input_text" placeholder="Password" />
                         </div>
                         <div className="login_form_button">
-                            <button className="login_form_button_text">Login</button>
+                            <button className="login_form_button_text"><h2>Login</h2></button>
                         </div>
                     </form>
+                    <div className='opForLogin'>
+                            <button className="login_Op"><h2>Department</h2></button>
+                            <button className="login_Op"><h2>Admin</h2></button>
+                    </div>
                 </div>
+               
             </div>
             <div className="login_sub_div"></div>
         </div>
+        
+       
         </div>
         </>
     );
